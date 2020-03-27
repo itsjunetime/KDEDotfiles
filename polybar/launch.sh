4894b6a3a998aa4e9420964aaee1bbde89f782e8
welkeri1 <iwmanticore8@gmail.com>
2020-03-26 21:37:07 -0600

    Grooved up the bar, made it a dock

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 ## Add this to your wm startup file.
 
@@ -8,12 +8,24 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2. Change HDMI2 to your other monitor(s)
+# Launch bar1 and bar2
 if polybar --list-monitors | grep HDMI2; then
 	env MONITOR=HDMI2 polybar main -c ~/.config/polybar/config.ini &
 fi
 polybar main -c ~/.config/polybar/config.ini &
+# ~/.config/polybar/scripts/spotify/launchlistener.sh
 
-# To start the spotify listener script
-~/.config/polybar/scripts/spotify/launchlistener.sh
+echo "setting c to zero"
+c=0
 
+while ! xdotool search -class 'polybar' ; do
+	echo "looping"
+        sleep 0.1
+        c=$((c+1))
+        if [ $c == 50 ] ; then
+                exit
+        fi
+done
+class=$(xdotool search -class 'polybar')
+echo $class
+xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $class
